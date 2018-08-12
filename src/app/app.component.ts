2c2ee788f95f02a116461fc2d65c3a0b4da92bdb
Alexnader Todorov <todorov@bezeqint.net>
2018-08-12 22:33:36 +0300

    PClist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
-
+interface pc {
+	name: string ; 
+	year: number ;
+	flag: string ;  
+}
+ interface cart {
+ 	items : number; 
+ 	typeName : string[] ; 
+ }
+var cartImg:string ;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+cartImg = "https://cdn4.iconfinder.com/data/icons/shopping-21/64/shopping-06-512.png";
+public pcList:pc[] = [
+{name:"Mac" , year: 1982, flag:"https://www.freeiconspng.com/uploads/apple-mac-icon-5.png"},
+{name:"Asus" , year: 2000,flag:"https://cdn2.iconfinder.com/data/icons/metro-uinvert-dock/256/Asus.png"},
+{name:"Dell" , year: 1972, flag:"https://cdn.iconscout.com/icon/free/png-256/dell-4-569248.png"},
+]; 
+
+public cart:cart = {items:0 , typeName:[]};
+
+buy(){
+this.cart.items ++
+
+}
+
+
   title = 'pcListHW';
 }
+
+
